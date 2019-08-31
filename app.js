bc36ddb83b6baf47d3ff295a7a91b56424a5065e
ucedoreTheBlue <53048635+ucedoreTheBlue@users.noreply.github.com>
2019-08-31 09:39:02 +0100

    more boids more starting positions more directions
    - there are now 160 boids
    - they can start at any x or y position
    - they have the same speed no matter what direction

--- app.js
+++ app.js
@@ -14,34 +14,42 @@ function renderBoids(x, y) {
     app.stage.addChild(boid);
     boid.x = x;
     boid.y = y;
-    let xspeed = 2;
-    let yspeed = 0;
-    let xcount = boid.x;
-    let ycount= boid.y;
+    let xspeed = Math.floor(Math.random()*5);
+    let yspeed = Math.sqrt((25-(xspeed*xspeed)));
+    let xcoordinate = boid.x;
+    let ycoordinate= boid.y;
 
     app.ticker.add(() => {
 
-        xcount += xspeed;
-        ycount += yspeed;
-        flightData[y/25].coordinates.pop();
-        flightData[y/25].coordinates.pop();
-        flightData[y/25].coordinates.push(xcount, ycount);
+        xcoordinate += xspeed;
+        ycoordinate += yspeed;
+        flightData[y/5].coordinates.pop();
+        flightData[y/5].coordinates.pop();
+        flightData[y/5].coordinates.push(xcoordinate, ycoordinate);
         boid.clear();
         boid.lineStyle(0, 0xff0000, 1);
         boid.beginFill(0xffFF00, 0.5);
 
-        if (xcount >= 800) {
+        if (xcoordinate >= 1000 || xcoordinate < -200) {
             boid.moveTo(boid.x, 40);
             boid.lineTo(boid.x + 5, 50);
             boid.lineTo(boid.x - 5, 50);
             boid.closePath();
-            xcount = 0;
+            xcoordinate = 0;
             boid.x = 0;
         }
+        else if (ycoordinate >= 1000 || ycoordinate < -20) {
+            boid.moveTo(boid.x, 40);
+            boid.lineTo(boid.x + 5, 50);
+            boid.lineTo(boid.x - 5, 50);
+            boid.closePath();
+            ycoordinate = 0;
+            boid.y = 0;
+        }
         else {
-            boid.moveTo(boid.x + xcount, 40);
-            boid.lineTo(boid.x + xcount + 5, 50);
-            boid.lineTo(boid.x + xcount - 5, 50);
+            boid.moveTo(boid.x + xcoordinate,ycoordinate+10);
+            boid.lineTo(boid.x + xcoordinate + 5, ycoordinate);
+            boid.lineTo(boid.x + xcoordinate - 5, ycoordinate);
             boid.closePath();
         }
 
@@ -51,9 +59,9 @@ function renderBoids(x, y) {
 }
 let flightData = {};
 let boidList = [];
-for (let i = 0; i < 800; i += 25) {
+for (let i = 0; i < 800; i += 5) {
     renderBoids(Math.floor(Math.random()*600), i);
-    boidList.push(i/25)
+    boidList.push(i/5)
 }
 
 for(let i = 0; i < boidList.length; i++){
@@ -63,5 +71,9 @@ for(let i = 0; i < boidList.length; i++){
     };
     console.log(flightData[i])
 }
-
-console.log("loading complete");
+              /*
+setInterval(() => {
+    console.log(flightData)
+}, 10);
+             */
+console.log("end of first loop");
