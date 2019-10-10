651ffd05ace70de4d2a7a6429db5becda8d4815e
ucedoreTheBlue <53048635+ucedoreTheBlue@users.noreply.github.com>
2019-10-10 14:25:56 +0100

    all distances between entities found

--- boids/app.js
+++ boids/app.js
@@ -8,28 +8,39 @@ const app = new PIXI.Application({
 });
 document.body.appendChild(app.view);
 
-coordinates = [];
-function renderBoids(x, y) {
+let flightData = {};
+let coordinates = [];
+let direction = [];
+let distances = [];
+
+function renderBoids(x, y, c) {
     const boid = new PIXI.Graphics();
     app.stage.addChild(boid);
     boid.x = x;
     boid.y = y;
     let xspeed = Math.random();
-    let yspeed = Math.sqrt((25-(xspeed*xspeed)));
+    let yspeed = Math.sqrt((25 - (xspeed * xspeed)));
     let xcoordinate = boid.x;
-    let ycoordinate= boid.y;
+    let ycoordinate = boid.y;
 
     app.ticker.add(() => {
 
         xcoordinate += xspeed;
         ycoordinate += yspeed;
-        flightData[y/5].coordinates.pop();
-        flightData[y/5].coordinates.pop();
+        flightData[y/5].coordinates.splice(0, 2);
         flightData[y/5].coordinates.push(xcoordinate, ycoordinate);
+        flightData[y/5].direction.pop();
+        flightData[y/5].direction.pop();
+        flightData[y/5].direction.push(xspeed, yspeed);
+        for (let i = 0; i < 160; i++) {
+            if (distances.length >= 160){
+                distances.pop()
+            }
+            distances.push(Math.sqrt( Math.pow((xcoordinate - xcoordinate*i*5), 2) +Math.pow((ycoordinate - ycoordinate*i*5), 2)));
+        }
         boid.clear();
-        boid.lineStyle(0, 0xff0000, 1);
+        boid.lineStyle(0, c, 1);
         boid.beginFill(0xffFF00, 0.5);
-
         if (xcoordinate >= 1000 || xcoordinate < -200) {
             boid.moveTo(boid.x, 40);
             boid.lineTo(boid.x + 5, 50);
@@ -37,44 +48,44 @@ function renderBoids(x, y) {
             boid.closePath();
             xcoordinate = 0;
             boid.x = 0;
-        }
-        else if (ycoordinate >= 1000 || ycoordinate < -20) {
+        } else if (ycoordinate >= 1000 || ycoordinate < 0) {
             boid.moveTo(boid.x, 40);
             boid.lineTo(boid.x + 5, 50);
             boid.lineTo(boid.x - 5, 50);
             boid.closePath();
             ycoordinate = 0;
             boid.y = -300;
-        }
-        else {
-            boid.moveTo(boid.x + xcoordinate,ycoordinate+10);
+        } else {
+            boid.moveTo(boid.x + xcoordinate, ycoordinate + 10);
             boid.lineTo(boid.x + xcoordinate + 5, ycoordinate);
             boid.lineTo(boid.x + xcoordinate - 5, ycoordinate);
             boid.closePath();
         }
-
-    });
-
-    boid.rotation = xspeed
-
+        boid.rotation = xspeed
+    })
 }
-let flightData = {};
+
 let boidList = [];
 for (let i = 0; i < 800; i += 5) {
-    renderBoids(Math.floor(Math.random()*600), i);
-    boidList.push(i/5)
+    renderBoids(Math.floor(Math.random() * 600), i, 0xffFF00);
+    boidList.push(i / 5)
 }
 
-for(let i = 0; i < boidList.length; i++){
+for (let i = 0; i < boidList.length; i++) {
+
     flightData[i] = {
         "ID": boidList[i],
-        "coordinates": coordinates
+        "coordinates": coordinates,
+        "direction": direction,
+        "distances": distances
     };
-    console.log(flightData[i])
 }
-/*
-setInterval(() => {
-    console.log(flightData)
-}, 10);
-  */
+
+
+setTimeout(() => {
+    for (let i = 0; i < boidList.length; i++) {
+        console.log(flightData[i]);
+    }
+}, 500);
+
 console.log("end of first loop");
